d9f8916e2bc0656c90a2b3d09fb52ea584174ad6
Peanut58 <1090709@students.wits.ac.za>
2020-02-17 15:25:55 +0200

    Adding code generating random 2D matrices of N = 10, 20 and 30

--- Matrix/2DMatrixAtt.py
+++ Matrix/2DMatrixAtt.py
@@ -5,10 +5,10 @@ import numpy as np
 np.random.seed()
 arr_A = np.zeros((1,3),dtype = np.ndarray)
 arr_B = np.zeros((1,3),dtype = np.ndarray)
-count = 2
+count = 10
 loc = 0
 #initialise array A and array B with 3 matrices with size 10, 20 and 30
-while count <=6:
+while count <=30:
 	arr_a = np.random.randint(0,100, size=(count,count))
 	arr_b = np.random.randint(0,100, size=(count,count))
 	print(arr_a)
@@ -16,7 +16,7 @@ while count <=6:
 	arr_A[0,loc] = [arr_a]
 	arr_B[0,loc] = [arr_b]
 	loc += 1
-	count += 2
+	count += 10
 
 #test to see if output is correct
 #print("Array A at first location")
@@ -29,21 +29,22 @@ while count <=6:
 #print (arr_A[0,2])
 
 #2D matrix multiplication. Answers will be stored in matrix C
-arr_C = np.zeros((1,3), dtype = np.ndarray)
-r = 0
+#I AM GETTING ERROS HERE - NOT TOO SURE IF THE MULTIPLICATION WORKS
+#arr_C = np.zeros((1,3), dtype = np.ndarray)
+#r = 0
 
-for r in range(2):
-	for i in range((len(arr_B[0,0])):
-		for j in range(len(arr_A[0,0])):
-			for k in range(len(arr_A[0,0])):
-				arr_C[0,r,i,j] += arr_A[0][r][i][k]*arr_B[0][r][k][j]
-				k += 1
-			j += 1
-		i += 1
-	#arr_C[0,r] = arr_c
-	r += 1
+#for r in range(2):
+#	for i in range((len(arr_B[0,0])):
+#		for j in range(len(arr_A[0,0])):
+#			for k in range(len(arr_A[0,0])):
+#				arr_C[0,r,i,j] += arr_A[0][r][i][k]*arr_B[0][r][k][j]
+#				k += 1
+#			j += 1
+#		i += 1
+	
+#	r += 1
 
-print("Array C in first location")
-print(arr_C[0,0])
+#print("Array C in first location")
+#print(arr_C[0,0])
 
 
