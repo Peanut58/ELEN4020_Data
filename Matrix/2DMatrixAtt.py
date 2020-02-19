ff8ea948dd5d1e264de9f1582d966914688d8dee
sansha-gupta <sansha.gupta99@gmail.com>
2020-02-19 12:47:20 +0200

    2D matrix multiplication

--- Matrix/2DMatrixAtt.py
+++ Matrix/2DMatrixAtt.py
@@ -12,7 +12,7 @@ while count <=30:
 	arr_a = np.random.randint(0,100, size=(count,count))
 	arr_b = np.random.randint(0,100, size=(count,count))
 	print(arr_a)
-	print(arr_b)
+	# print(arr_b)
 	arr_A[0,loc] = [arr_a]
 	arr_B[0,loc] = [arr_b]
 	loc += 1
@@ -21,7 +21,7 @@ while count <=30:
 #test to see if output is correct
 #print("Array A at first location")
 #print (arr_A[0,0])
-#print("Array A first index of array at first location") 
+#print("Array A first index of array at first location")
 #print(arr_A[0][0][0][0])
 #print("Array A at second location")
 #print (arr_A[0,1])
@@ -30,21 +30,24 @@ while count <=30:
 
 #2D matrix multiplication. Answers will be stored in matrix C
 #I AM GETTING ERROS HERE - NOT TOO SURE IF THE MULTIPLICATION WORKS
-#arr_C = np.zeros((1,3), dtype = np.ndarray)
-#r = 0
+arr_C = np.zeros((1,3), dtype = np.ndarray)
+r = 0
 
-#for r in range(2):
-#	for i in range((len(arr_B[0,0])):
-#		for j in range(len(arr_A[0,0])):
-#			for k in range(len(arr_A[0,0])):
-#				arr_C[0,r,i,j] += arr_A[0][r][i][k]*arr_B[0][r][k][j]
-#				k += 1
-#			j += 1
-#		i += 1
-	
-#	r += 1
+print("--------- matrix multiplication ------------")
+for r in range(2):
+	r += 1
 
-#print("Array C in first location")
-#print(arr_C[0,0])
+# print("Array C in first location")
+# print(arr_C[0,0])
+
+print("len of A", len(arr_A[0][0][0]))
 
 
+# for i in range(0,(len(arr_B[0,0]))):
+# 			for j in range(len(arr_A[0,0])):
+# 				for k in range(len(arr_A[0,0])):
+# 					arr_C[0,r,i,j] += arr_A[0][r][i][k]*arr_B[0][r][k][j]
+
+# 					k += 1
+# 				j += 1
+# 			i += 1
