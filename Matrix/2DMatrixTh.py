297728bcc832670a33e4375de5f1b6065e511cf2
Peanut58 <1090709@students.wits.ac.za>
2020-02-21 12:02:07 +0200

    2DMatrixTh.py

--- Matrix/2DMatrixTh.py
+++ Matrix/2DMatrixTh.py
@@ -1,5 +1,6 @@
 import threading 
 import numpy as np 
+import time
 
 def rank2TensorMultOpenMP(A, B, C): 
     for i in range(0, len(A)):
@@ -9,6 +10,7 @@ def rank2TensorMultOpenMP(A, B, C):
     return C
 
 N = [10,20,30]
+noThreads = 2
 for n in N:
     print("N is ",n)
     A = np.random.randint(0, 100, size=(n, n))
@@ -16,8 +18,10 @@ for n in N:
     C = np.zeros((n, n), dtype=int)
 
     threads = []
-    for threadNum in range(2):
+    t1 = time.perf_counter()
+    for threadNum in range(noThreads):
         # print("thread number " , threadNum)
+        
         x = threading.Thread(target=rank2TensorMultOpenMP, args=(A,B,C,))
         threads.append(x)
         # print(threading.enumerate())
@@ -25,5 +29,7 @@ for n in N:
 
     for threadNum, thread in enumerate(threads):
         thread.join()
-    
+    t2 = time.perf_counter()
+
+    print(f"Time taken for a %s x %s with Python MP was {t2 - t1:0.4f} sec for %s threads"%(n,n,noThreads))
         
